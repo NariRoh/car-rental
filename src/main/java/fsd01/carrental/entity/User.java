f55146d9170d987a67337b5811dc155552d95a1d
Nari Roh <nari.rohh@gmail.com>
2021-12-09 18:46:13 -0500

    feat: add mypostgreSQL dependency

--- src/main/java/fsd01/carrental/entity/User.java
+++ src/main/java/fsd01/carrental/entity/User.java
@@ -1,5 +1,34 @@
 package fsd01.carrental.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    
+    @Email
+    private String email;
+    private String password;
+    private Integer phoneNumber;
+    
+
 
 }
