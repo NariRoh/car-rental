1361dff9935ea3bd443b9ebcbd3d07daa5d21d83
latchmanc6 <latchmanc6@gmail.com>
2021-12-12 11:44:30 -0500

    feat: finished js and server search bar

--- src/main/java/fsd01/carrental/repository/CarRepository.java
+++ src/main/java/fsd01/carrental/repository/CarRepository.java
@@ -16,4 +16,9 @@ public interface CarRepository extends JpaRepository<Car, Long> {
     @Query("select c from Car c")
     List<Car> getListOfCars();
 
+    @Query("select c from Car c where c.dailyRate between ?1 and ?2 and c.transmission like ?3 and c.mileage between ?4 and ?5 and c.fuelType like ?6 and c.seats between ?7 and ?8")
+    List<Car> getListOfCarsSearch(double minPrice, double maxPrice, String transmission, int minMileage, int maxMileage, String fuel, int minSeats, int maxSeats);
+
+    @Query("select c from Car c order by c.noBookings desc")
+    List<Car> getIndexFeaturedCars();
 }
