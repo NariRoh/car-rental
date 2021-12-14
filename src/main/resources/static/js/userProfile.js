0f871e089f59472dd41c6d79f33c706e660654f0
Nari Roh <nari.rohh@gmail.com>
2021-12-13 21:17:11 -0500

    feat: apply jQuery to hide and show the form

--- src/main/resources/static/js/userProfile.js
+++ src/main/resources/static/js/userProfile.js
@@ -1,22 +1,39 @@
 $(document).ready(function() {
     console.log("loading jQuery functions");
 
+    $("#userPassword").hide();
+
+    $("#changePass").click(function (e) {
+        $("#userPassword").show();
+        $("#userProfile").hide();
+    });
+
+    $("#changeProfile").click(function () {
+        $("#userProfile").show();
+        $("#userPassword").hide();
+    });
+        //     const parentNode = $(e.target).parents(".user-info");
+        //
+        //     parentNode.siblings(".hidden-form").show();
+        //     parentNode.hide();
+        // });
+
     // show edit form to edit
-    $(".btn-edit").click(function (e) {
-        const parentNode = $(e.target).parents(".user-info");
-
-        parentNode.siblings(".hidden-form").show();
-        parentNode.hide();
-    });
-
-    // hide edit form when cancel button clicked
-    $(".btn-cancel").click(function (e) {
-        e.preventDefault();
-        const parentNode = $(e.target).parents(".hidden-form");
-
-        parentNode.hide();
-        parentNode.siblings(".user-info").show()
-    });
+    // $(".btn-edit").click(function (e) {
+    //     const parentNode = $(e.target).parents(".user-info");
+    //
+    //     parentNode.siblings(".hidden-form").show();
+    //     parentNode.hide();
+    // });
+    //
+    // // hide edit form when cancel button clicked
+    // $(".btn-cancel").click(function (e) {
+    //     e.preventDefault();
+    //     const parentNode = $(e.target).parents(".hidden-form");
+    //
+    //     parentNode.hide();
+    //     parentNode.siblings(".user-info").show()
+    // });
 
     // $("#profileForm").submit(function (e) {
     //    e.preventDefault();
