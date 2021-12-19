28a600c54454485bbbfdd31cf4bc2f3ec1dfcec4
latchmanc6 <latchmanc6@gmail.com>
2021-12-19 12:27:10 -0500

    feat: confirmation email

---
+++ src/main/java/fsd01/carrental/service/EmailSenderService.java
@@ -0,0 +1,39 @@
+package fsd01.carrental.service;
+
+import fsd01.carrental.model.Mail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class EmailSenderService {
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Autowired
+    private SpringTemplateEngine templateEngine;
+
+    public void sendEmail(Mail mail) throws MessagingException, IOException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+        Context context = new Context();
+        context.setVariables(mail.getProps());
+
+        String html = templateEngine.process("email-template", context);
+        helper.setTo(mail.getMailTo());
+        helper.setText(html, true);
+        helper.setSubject(mail.getSubject());
+        helper.setFrom(mail.getFrom());
+        emailSender.send(message);
+    }
+}
